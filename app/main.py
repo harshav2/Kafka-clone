e27dec8ca5b1c0ba7aea0df09d88f056010a984e
Harsha V <harsha.vishwanath2004@gmail.com>
2025-01-11 18:49:12 +0530

    refactoring of api_versions

--- app/main.py
+++ app/main.py
@@ -1,26 +1,34 @@
 import socket  # noqa: F401
 
-def create_message(message):
+def api_versions(message):
     correlation_id_bytes = message[8:12]
     request_api_version = message[6:8]
+    api_key = message[4:6]
 
-    if b'0000' <= request_api_version <= b'0004':
-        response_code = 0
+    min_version = bytes([0x00, 0x00]) 
+    max_version = bytes([0x00, 0x04]) 
+
+    if min_version <= request_api_version <= max_version:
+        error_code = 0
     else:
-        response_code = 35
+        error_code = 35
 
-    response_code_bytes = response_code.to_bytes(2, byteorder="big")
+    error_code_bytes = error_code.to_bytes(2, byteorder="big")
     
-    message_bytes = correlation_id_bytes + response_code_bytes
+    message_bytes = correlation_id_bytes + error_code_bytes + api_key + min_version + max_version
 
     message_size = len(message_bytes).to_bytes(4, byteorder="big") #byte order refers to byte endian
 
     return message_size+message_bytes
 
+def create_message(message):
+    request_api_key = message[4:6]
+    if request_api_key == bytes([0x00, 0x12]):
+        return api_versions(message)
 
 def handleClient(client):
-    output=client.recv(1024)
-    client.sendall(create_message(message=output))
+    message = client.recv(1024)
+    client.sendall(create_message(message))
 
 def main():
     print("Logs from your program will appear here!")
